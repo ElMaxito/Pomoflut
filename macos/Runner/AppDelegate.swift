c1f2810ef76cb6d34389a79b52d18be76aacbfc1
ElMaxito <113039423+ElMaxito@users.noreply.github.com>
2024-05-22 12:28:35 +0200

    Pre-Release Version
    Finetuned some things, reverted some code. Should now work as intended.

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,16 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationDidFinishLaunching(_ aNotification: Notification) {
+    let window = NSApplication.shared.windows.first
+    let initialSize = NSSize(width: 300, height: 436)
+    
+    window?.minSize = initialSize // Set minimum size
+    window?.maxSize = initialSize // Set maximum size
+
+    // Set the initial size
+    window?.setFrame(NSRect(origin: window!.frame.origin, size: initialSize), display: true)
+  }
 }
+
